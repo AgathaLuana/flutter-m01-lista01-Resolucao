f83232ebd8919b989b365cb30cc42504b758225e
Ágatha <agathalua.al@gmail.com>
2022-01-07 21:22:47 -0300

    first commit

---
+++ bin/14_menu_crud.dart
@@ -0,0 +1,20 @@
+/*Construir um Menu de opções para gerenciar (manter) os registros dos usuários do sistema.
+Instruções
+Escreva um algoritmo em Dart que construa um menu de cadastro com as opções:
+C) Create	(Cadastrar/Inserir)
+R) Read		(Buscar/Ler)
+U) Update	(Editar/Atualizar)
+D) Delete	(Excluir/Apagar)
+Mostre a opção escolhida pelo usuário.
+Casos de Testes
+Entrada:
+  r (ou R)
+
+Saída:
+  "Buscar/Ler"*/
+void main(){
+  
+
+
+
+}
---
+++ bin/21_quadrado_impares.dart
@@ -0,0 +1,4 @@
+/*Mostrar o quadrado de todos os números ímpares, de 1 até 200.
+
+Instruções
+Escreva um algoritmo em Dart que apresente o quadrado de todos os números ímpares, de 1 até 200.*/
